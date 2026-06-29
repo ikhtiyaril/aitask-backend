4fb1cb4d207b8a26a4a27b2cdf61f8aab8b70afe
Mac <mac@MacBook-M1-Pro.local>
2026-06-29 20:52:18 +0700

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const ports = process.env.PORT || 5000
+const ports = process.env.PORT || 3000
 
 app.listen(ports, ()=> {
     console.log(`Sistem Berjalan Di Port ${ports}`)

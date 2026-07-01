d41cf6c19f219474945061fe2e00c5639f504343
Mac <mac@MacBook-M1-Pro.local>
2026-07-01 14:22:07 +0700

    fitur login belum fix

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const connectDb = require('./config/database')
 
 const ports = process.env.PORT || 3000
 
+
+app.use('/api/login',require('./routes/loginRoutes'))
+
+connectDb()
 app.listen(ports, ()=> {
     console.log(`Sistem Berjalan Di Port ${ports}`)
 })

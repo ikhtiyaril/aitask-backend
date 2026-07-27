65e54ea4058e2ac8893f1ec5bbf744d70b8b8838
Mac <mac@MacBook-M1-Pro.local>
2026-07-27 12:15:23 +0700

    refresh

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+require("dotenv").config()
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const connectDb = require('./config/database')
 
 const ports = process.env.PORT || 3000
+app.use(express.json())
 
+app.use(cors({
 
-app.use('/api/login',require('./routes/loginRoutes'))
+    origin: "http://localhost:5173",
+    credentials: true
+
+}))
+
+app.use('/api/auth',require('./routes/loginRoutes'))
 
 connectDb()
+
 app.listen(ports, ()=> {
     console.log(`Sistem Berjalan Di Port ${ports}`)
 })
